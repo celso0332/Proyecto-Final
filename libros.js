bfd5a882ed96180df3e1e91a2d2df17968fcdfdc
celso0332 <celso.carrascocervantescesunbc.edu.mx>
2022-08-09 21:01:38 -0600

    proyecto PoP 4

--- libros.js
+++ libros.js
@@ -6,37 +6,44 @@ require('./mongooseconections');
 const Libro =require("./Models/libros")
 
 const Inventario =require("./Models/Inventario")
-describe("Operaciones con libros", function(){
+describe("Operaciones con inventarios", function(){
     describe("Como supervisor y almacenista de sucursal podrán crear los inventarios de su sucursal", function(){
         it("crear nuevo inventario", function(){
             var listaLibros=["libro1", "libro2"]
             var datos=[numero=1,supervisor="supervisor1",lista=listaLibros]
             const crear= async function(){
-                try {
-                    await Libro.create({
-                        numero:datos.numero,
-                        supervisor:datos.supervisor,
-                        listaLibros:datos.lista 
+               try{
+                    await Inventario.create({
+                        numero:1,
+                        supervisor:"supervisor1",
+                        listaLibros:["libro1","libro2"] 
                     })
                   
                     return 1
-                } catch (error) {
-                    return 0;
                 }
-                
-                
+                catch(error){
+                    return 0
+                }
             }
             const CrearInventario=function(){
-              
-              if(crear!=0){
+             try {
+                crear()
                 return 1
-               }else{
+             } catch (error) {
                 return 0
-               }
+             }
             }
             assert.equal(CrearInventario(), 1)
-            
-            
         })
+        
+    })
+    describe("Como almacenistas podrán agregar y editar libros dentro del inventario", function(){
+        it("Agregar libros nuevos",async function(){
+            const inventario=await Inventario.find()
+            console.log(inventario)
+
+            assert.equal(1,1)
+        })
+
     })
 })
